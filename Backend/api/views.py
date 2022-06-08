a5988c7ee14d8656733ebddb5a3247c7f912fc17
Erwing <erwingdelfines@hotmail.com>
2022-06-08 02:18:31 -0500

    api-view-drf

--- Backend/api/views.py
+++ Backend/api/views.py
@@ -1,8 +1,18 @@
-import json
-from django.http import JsonResponse
+from django.forms.models import model_to_dict
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+from products.models import Product
 
 
+@api_view(["GET"])
 def api_home(request, *args, **kwargs):
+    model_data = Product.objects.all().order_by("?").first()
+    data = {}
+    if model_data:
+        data = model_to_dict(model_data, fields=["id", "price"])
+
+    """" 
     body = request.body  # byte string of json
     data = {}
     print(request.GET)
@@ -12,7 +22,11 @@ def api_home(request, *args, **kwargs):
         pass
 
     print(data)
-    #data['headers'] = request.headers
-    data['content-type'] = dict(request.content_type)
+
+
+    #data['headers'] = dict(request.headers)
+    #data['content-type'] = dict(request.content_type)
     data["params"] = dict(request.GET)
-    return JsonResponse(data)
+    """
+
+    return Response(data)
