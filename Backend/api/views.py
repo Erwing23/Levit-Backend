b34d1500d229dd07cc24c885bb059594cb549625
Erwing <erwingdelfines@hotmail.com>
2022-06-12 01:03:27 -0500

    Serializers

--- Backend/api/views.py
+++ Backend/api/views.py
@@ -1,18 +1,33 @@
+import re
 from django.forms.models import model_to_dict
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
 from products.models import Product
+from products.serializers import ProductSerializer
 
 
-@api_view(["GET"])
+@api_view(["POST"])
 def api_home(request, *args, **kwargs):
-    model_data = Product.objects.all().order_by("?").first()
-    data = {}
-    if model_data:
-        data = model_to_dict(model_data, fields=["id", "price"])
 
-    """" 
+    serializer = ProductSerializer(data=request.data)
+    if serializer.is_valid(raise_exception=True):
+       # instance = serializer.save()
+        print(serializer.data)
+        return Response(serializer.data)
+    return Response({"invalid": "Not good Data"}, status=400)
+    """ 
+    Serialize Models añadiendo atributos a traves de procesos del mismo modelo
+    instance = Product.objects.all().order_by("?").first()
+    data = {}
+    if instance:
+        #data = model_to_dict(model_data, fields=["id", "price", "sale_price"])
+        #data["sale_price"] += 12
+        data = ProductSerializer(instance).data
+    """
+
+    """ 
+    GET METHOD REQUEST
     body = request.body  # byte string of json
     data = {}
     print(request.GET)
@@ -28,5 +43,3 @@ def api_home(request, *args, **kwargs):
     #data['content-type'] = dict(request.content_type)
     data["params"] = dict(request.GET)
     """
-
-    return Response(data)
--- pyclient/basic.py
+++ pyclient/basic.py
@@ -3,8 +3,8 @@ import requests
 
 endpoint = "http://localhost:8000/api/"
 
-get_response = requests.get(
-    endpoint, params={"param1": "value1"}, json={"1": "Hello"})  # API request sender
+get_response = requests.post(
+    endpoint,  json={"title": "Hello World", "price": 123})  # API request sender
 # print(get_response.text)
 # print(get_response.status_code)
 print(get_response.json())  # Response from the API
